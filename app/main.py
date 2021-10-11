28e97d3bbf26cac91553c651f05081e788dac2ee
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-10-11 10:48:24 +0330

    Init of Flask

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '1234'
+
+api = requests.get("https://data.covidapi.com/countries").json()
+
+@app.route("/")
+def index():
+    return "Hi"
