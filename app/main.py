88beab5726723aa14f6353426b42c94f0720c1f4
erfansmart12 <erfansmart12@gmail.com>
2021-10-12 17:53:26 +0330

    Sorting codes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, redirect, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
-
-class GetUsername(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-
 import requests
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1234'
+
+
+class GetUsername(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -20,6 +20,7 @@ def index():
         username = form.username.data
         return redirect((f'/user?username={username}'))
     return render_template('index.html', form=form)
+
 
 @app.route("/user", methods = ["GET", "POST"])
 def user():
